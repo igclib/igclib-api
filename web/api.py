02b227e2888f0ed67ce0fe1050ae97e6bae8f11a
Téo Bouvard <teobouvard@gmail.com>
2020-04-12 13:09:18 +0200

    Add external xc optimization

--- web/api.py
+++ web/api.py
@@ -5,50 +5,71 @@ import json
 from flask import Flask, jsonify, redirect, request, send_file
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config['JSON_AS_ASCII'] = False
-app.config['DEBUG'] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.config["JSON_AS_ASCII"] = False
+app.config["DEBUG"] = True
 
 
-@app.route('/')
-@app.route('/api')
+@app.route("/")
+@app.route("/api")
 def index():
-    return send_file('static/index.html')
+    return send_file("static/index.html")
 
 
-@app.route('/api/xc', methods=['GET', 'POST'])
+@app.route("/api/xc", methods=["GET", "POST"])
 def xc():
-    if request.method == 'GET':
-        return send_file('static/airspace_validation.html')
+    if request.method == "GET":
+        return send_file("static/airspace_validation.html")
 
-    if 'flight' not in request.files:
+    if "flight" not in request.files:
         return redirect(request.url)
 
-    flight = request.files['flight']
+    flight = request.files["flight"]
     tf_flight = NamedTemporaryFile(suffix=flight.filename)
     flight.save(tf_flight.name)
 
     tf_airspace = None
-    if 'airspace' not in request.files or not request.files['airspace'].filename:
-        airspace_name = os.environ.get('AIRSPACE_FILE', '')
+    if "airspace" not in request.files or not request.files["airspace"].filename:
+        airspace_name = os.environ.get("AIRSPACE_FILE", "")
     else:
         tf_airspace = NamedTemporaryFile()
-        airspace = request.files['airspace']
+        airspace = request.files["airspace"]
         airspace.save(tf_airspace.name)
         airspace_name = tf_airspace.name
 
     try:
-        result = check_output([f'{os.environ["HOME"]}/dev/igclib/build/src/igclib', 'xc',
-                               '--flight', tf_flight.name, '--airspace', airspace_name])
-        result = json.loads(result)
-        return jsonify(result)
+        airspace_result = check_output(
+            [
+                # igclib executable
+                f'{os.environ["HOME"]}/dev/igclib-stable/build/src/igclib',
+                "xc",
+                "--flight",
+                tf_flight.name,
+                "--airspace",
+                airspace_name,
+            ]
+        )
+
+        airspace_result = json.loads(airspace_result)
+        xc_result = check_output(
+            [
+                # hspoints executable
+                f'{os.environ["HOME"]}/usr/local/bin/xc_optimizer',
+                tf_flight.name,
+                "-",
+                "XC",
+            ]
+        )
+        xc_result = json.loads(xc_result)
+        airspace_result["xc_info"] = xc_result
+        return jsonify(airspace_result)
     except CalledProcessError:
-        return jsonify({'error': 'bad igc file'})
+        return jsonify({"error": "bad igc file"})
     finally:
         tf_flight.close()
         if tf_airspace is not None:
             tf_airspace.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
